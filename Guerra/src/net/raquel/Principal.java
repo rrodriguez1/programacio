8451231bdc758f2dd0b324abb7d3555c51bc28d9
raquel <raquel@raquel-Aspire-5610>
2014-10-27 14:30:33 +0100

    Primers arxius de l'exercici

--- Guerra/src/net/raquel/Principal.java
+++ Guerra/src/net/raquel/Principal.java
@@ -1,5 +1,41 @@
+/**
+ *
+ */
 package net.raquel;
 
-public class Principal {
+import acm.graphics.GImage;
+import acm.program.GraphicsProgram;
+import java.util.ArrayList;
+/**
+ * @author Raquel
+ *
+ */
+public class Principal extends GraphicsProgram {
+ArrayList<LLUITADORESQ> guerreros;
+ArrayList<LLUITADORDRET> peleones;
+public void run(){
+setSize(600,600);
+guerreros = new ArrayList();
+peleones = new ArrayList();
+int posicion = 0;
+for (int i = 0 ; i < 10;i++){
+	guerreros.add(new LLUITADORESQ(i,posicion));
+	peleones.add(new LLUITADORDRET(i,posicion));
+	posicion = posicion + 50;
+}
+
+for (int i = 0; i < guerreros.size() ; i++){
+	add(guerreros.get(i).getImatge());
+	add(peleones.get(i).getImatge());
+}
+while(1 >0){
+for (int i = 0; i < guerreros.size(); i++){
+
+	guerreros.get(i).getImatge().move(5, 0);
+	peleones.get(i).getImatge().move(-5, 0);
+
 
 }
+pause(100);
+}
+}}
